109e248283388a8ad5bb4b896a7636cbea6bd939
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-01-04 12:51:58 -0300

    Add logout to CORS routes.

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'register', 'login'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'register', 'login', 'logout'],
 
     'allowed_methods' => ['*'],
 
