c1ba39189c82c55fd36121a305982348d2446fc0
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-01-10 13:52:59 -0300

    User register validation tests.

--- tests/Feature/Auth/AuthUserTest.php
+++ tests/Feature/Auth/AuthUserTest.php
@@ -48,6 +48,54 @@ class AuthUserTest extends TestCase
   }
 
   /** @test */
+  public function all_fields_are_required_when_register_user()
+  {
+    $response = $this->postJson('/register', []);
+
+    $response->assertJsonValidationErrors(['name', 'email', 'password']);
+  }
+
+  /** @test */
+  public function password_confirmation_is_required_and_must_match()
+  {
+    $response = $this->postJson('/register', [
+      'name' => 'mateus',
+      'email' => 'teste@teste.com',
+      'password' => 'password',
+    ]);
+
+    $response->assertJsonValidationErrors(['password']);
+  }
+
+  /** @test */
+  public function email_must_be_unique()
+  {
+    User::factory()->create(['email' => 'teste@teste.com']);
+
+    $response = $this->postJson('/register', [
+      'name' => 'mateus',
+      'email' => 'teste@teste.com',
+      'password' => 'password',
+      'password_confirm' => 'password'
+    ]);
+
+    $response->assertJsonValidationErrors(['email' => 'The email has already been taken.']);
+  }
+
+  /** @test */
+  public function password_must_be_greater_than_8_characters()
+  {
+    $response = $this->postJson('/register', [
+      'name' => 'mateus',
+      'email' => 'teste@teste.com',
+      'password' => 'pass',
+      'password_confirm' => 'pass'
+    ]);
+
+    $response->assertJsonValidationErrors(['password' => 'The password must be at least 8 characters.']);
+  }
+
+  /** @test */
   public function invalid_credentials_returns_error()
   {
     User::factory()->create();
