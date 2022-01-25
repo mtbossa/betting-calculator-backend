a4869d0b92618a86ebacc76887c9f97376da55e5
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-01-25 20:12:38 -0300

    IDE Helper.

--- app/Models/Bet.php
+++ app/Models/Bet.php
@@ -7,6 +7,37 @@ use App\Traits\Multitenantable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Bet
+ *
+ * @property int $id
+ * @property int $betted_team
+ * @property float $odd
+ * @property float $amount
+ * @property float $profit
+ * @property float $real_profit
+ * @property int $match_id
+ * @property int $user_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\BetableMatch $match
+ * @property-read \App\Models\User $user
+ * @method static \Database\Factories\BetFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Bet newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Bet newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Bet query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Bet whereAmount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Bet whereBettedTeam($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Bet whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Bet whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Bet whereMatchId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Bet whereOdd($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Bet whereProfit($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Bet whereRealProfit($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Bet whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Bet whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Bet extends Model
 {
   use HasFactory, Multitenantable;
--- app/Models/BetableMatch.php
+++ app/Models/BetableMatch.php
@@ -7,6 +7,32 @@ use App\Traits\Multitenantable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\BetableMatch
+ *
+ * @property int $id
+ * @property string $team_one
+ * @property string $team_two
+ * @property int|null $winner_team
+ * @property int $user_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Bet[] $bets
+ * @property-read int|null $bets_count
+ * @property-read \App\Models\User $user
+ * @method static \Database\Factories\BetableMatchFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|BetableMatch newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|BetableMatch newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|BetableMatch query()
+ * @method static \Illuminate\Database\Eloquent\Builder|BetableMatch whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BetableMatch whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BetableMatch whereTeamOne($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BetableMatch whereTeamTwo($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BetableMatch whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BetableMatch whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|BetableMatch whereWinnerTeam($value)
+ * @mixin \Eloquent
+ */
 class BetableMatch extends Model
 {
   use HasFactory, Multitenantable;
