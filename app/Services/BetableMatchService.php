402b38ceb81ed86b7543f0cd26dc582ee1fc30df
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-02-15 20:07:31 -0300

    refactor betablematch index() method

--- app/Services/BetableMatchService.php
+++ app/Services/BetableMatchService.php
@@ -2,21 +2,34 @@
 
 namespace App\Services;
 
-use App\Http\Requests\StoreBetableMatchRequest;
-use App\Http\Requests\UpdateBetableMatchRequest;
 use App\Models\BetableMatch;
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Symfony\Component\HttpFoundation\Response;
 
 class BetableMatchService {
-  public function index(Request $request): Collection
+  public function index(Request $request): \Illuminate\Support\Collection
+  {
+    $query = $this->_filterMatchesQuery($request);
+
+    return $query->get();
+  }
+
+  private function _filterMatchesQuery(Request $request): Builder
   {
     $query = BetableMatch::query();
 
     $query->when($request->boolean('with_bets'), function ($q) {
       $q->with('bets');
+    });
+
+    $query->when($request->date('start_date'), function($q, $start_date) {
+      $test = $start_date;
+      $q->whereDate('created_at', '>=', $start_date);
+    });
+
+    $query->when($request->date('end_date'), function($q, $end_date) {
+      $test = $end_date;
+      $q->whereDate('created_at', '<=', $end_date);
     });
 
     if($request->has('match_finished')) {
@@ -29,6 +42,6 @@ class BetableMatchService {
       });
     }
 
-    return $query->get();
+    return $query;
   }
 }
